44a35c7c884b9ed42d03aeb763001b561a4ccc33
ZEROZ0NE <melle.2014@hotmail.com>
2019-11-14 20:05:26 +0100

    nog klar

--- databasefile.sql
+++ databasefile.sql
@@ -20,21 +20,21 @@ CREATE TABLE historik
 			 Konto_hist INT,
 			 Datum DATE NOT NULL,
 			 Prod_hist INT,
-FOREIGN KEY(Konto_hist) REFERENCES konto(Person_ID),
-FOREIGN KEY(Prod_hist) REFERENCES produkt(Produkt_ID)
+FOREIGN KEY(Konto_hist) REFERENCES konto(Person_ID) ON DELETE CASCADE,
+FOREIGN KEY(Prod_hist) REFERENCES produkt(Produkt_ID) ON DELETE SET NULL
 );
 CREATE TABLE vaurkorg
 			(Konto_varu INT,
 			 Produkt_varu INT,
-FOREIGN KEY(Konto_varu) REFERENCES konto(Person_ID),
-FOREIGN KEY(Produkt_varu) REFERENCES produkt(Produkt_ID)
+FOREIGN KEY(Konto_varu) REFERENCES konto(Person_ID) ON DELETE CASCADE,
+FOREIGN KEY(Produkt_varu) REFERENCES produkt(Produkt_ID) ON DELETE CASCADE
 );
 
 CREATE TABLE kommentarer
 			(Person_ID INT, 
 			Produkt_ID INT,
 			kommentar VARCHAR(100),
-			FOREIGN KEY(Person_ID) REFERENCES konto(Person_ID),
-			FOREIGN KEY(Produkt_ID) REFERENCES produkt(Produkt_ID)
+			FOREIGN KEY(Person_ID) REFERENCES konto(Person_ID)ON DELETE SET NULL,
+			FOREIGN KEY(Produkt_ID) REFERENCES produkt(Produkt_ID) ON DELETE CASCADE
 );
 
